51d0b71e33df9b52a95bd7329a50085229e3afb2
Sebastian H. <maracuchoamericano@gmail.com>
2025-01-25 18:40:59 -0500

    back mostly complete

--- app.js
+++ app.js
@@ -11,15 +11,22 @@ const Cryptr = require('cryptr');
 const session = require("express-session");
 const MemoryStore = require('memorystore')(session)
 const fs = require("fs")
+const csv = require('csv-parser')
 const https = require("https")
 const WebSocket = require("ws");
+const { equal } = require('assert');
 
+
+const saltRounds = 10;
 
 
 
 
 
 const app = express();
+
+
+
 
 
 
@@ -40,6 +47,8 @@ app.use(session({
         checkPeriod: 86400000 // prune expired entries every 24h
     }), 
 }));
+
+app.use(bodyParser.json({limit: "10mb"}))
 
 function authenticateUser(req) {
 
@@ -82,7 +91,7 @@ if (process.env.NODE_ENV === "DEV") {
     };
 
     app.use(cors({
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true
     }))
@@ -92,8 +101,6 @@ if (process.env.NODE_ENV === "DEV") {
 
 console.log(options)
 const server = https.createServer(options, app);
-
-
 
 
 // mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@hoyahacks.88863.mongodb.net/`);
@@ -108,6 +115,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    name: {
+        type: String,
+        required: false
+    },
     email: {
         index: true,
         type: String,
@@ -117,7 +128,38 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         index: true
+    },
+    password: {
+        type: String,
+        required: true,
     }
+})
+
+
+const campaignSchema = new mongoose.Schema({
+    uuid: {
+        type: String,
+        index: true
+    },
+    jobListing: {
+        type: String,
+        unique: false,
+    },
+    applicants: {
+        type: Array,
+        unique: false
+    },
+    numberOfApplicants: {
+        type: Number,
+        unique: false,
+    },
+    criteria: {
+        type: String,
+        unique: false,
+    }
+
+
+
 })
 
 
@@ -125,6 +167,450 @@ const userSchema = new mongoose.Schema({
 
 
 const User = new mongoose.model("User", userSchema)
+const Campaign = new mongoose.model("Campaign", campaignSchema)
+
+
+
+
+
+
+app.post("/signup", (req,res) => {
+    const {name, email, password} = req.body;
+
+    let inputs = {};
+
+
+    if (name && email && password) {
+        inputs = {
+            name: name.trim().toLowerCase(),
+            email: email.trim().toLowerCase(),
+            password: password.trim().toLowerCase(),
+        }
+    } else {
+        res.status(400).send(JSON.stringify({
+            code: "err",
+            message: "invalid request"
+        }))
+    }
+
+    
+    
+    console.log(inputs)
+    authenticateUser(req).then((id) => {
+        if (id === "No user found") {
+            // This is expected
+
+            User.findOne({emailHash: md5(inputs.email)}).then(async(user,err) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(JSON.stringify({
+                        code: "err",
+                        message: "invalid request"
+                    }))
+                } else {
+
+                    if (user) {
+                        req.session.user = user.uuid;
+                        res.status(200).send(JSON.stringify({
+                            code: "ok",
+                            message: "account exists"
+                        }))
+                    } else {
+
+                        const uuid = uuidv4()
+
+                        bcrypt.hash(inputs.password, saltRounds, async function(err, hash) {
+                            if (err) {
+                                console.log(err)
+                                res.status(400).send(JSON.stringify({
+                                    code: "err",
+                                    message: "invalid request"
+                                }))
+                            } else {
+
+                                const newUser = new User({
+                                    uuid: uuid,
+                                    name: cmod.encrypt(inputs.name),
+                                    email: cmod.encrypt(inputs.email),
+                                    emailHash: md5(inputs.email),
+                                    password: hash
+        
+        
+                                })
+                                
+                                await newUser.save(); 
+                                req.session.user = uuid;
+
+                        
+
+
+                                res.status(200).send(JSON.stringify({
+                                    code: "ok",
+                                    message: "user created"
+                                }))
+      
+
+                            }
+                        });
+                        
+
+                        
+
+                       
+
+                    }
+
+
+                }
+            })
+
+
+
+
+
+
+
+        } else {
+            res.status(200).send(JSON.stringify({
+                code: "ok",
+                message: "logged in"
+            }))
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+app.post("/login", (req,res) => {
+    const {email, password} = req.body;
+
+    if (email && password) {
+        User.findOne({emailHash: md5(email)}).then((user,err) => {
+            if (err) {
+                console.log(err)
+                res.status(400).send(JSON.stringify({
+                    code: "err",
+                    message: "invalid request"
+                }))
+            } else {
+                if (user !== null) {
+
+
+
+                    bcrypt.compare(password, user.password, function(err, result) {
+
+                        if (err) {
+                            console.log(err)
+                            res.status(400).send(JSON.stringify({
+                                code: "err",
+                                message: "invalid request"
+                            }))
+                        } else {
+                            if (result) {
+                                req.session.user = user.uuid
+                                res.status(200).send(JSON.stringify({
+                                    code: "ok",
+                                    message: "success"
+                                }))
+                            } else {
+                                res.status(403).send(JSON.stringify({
+                                    code: "err",
+                                    message: "invalid credentials"
+                                }))
+                            }
+                        }
+                        // result == true
+                    });
+                    
+
+                    
+
+
+
+
+                } else {
+
+                    res.status(403).send(JSON.stringify({
+                        code: "err",
+                        message: "invalid credentials"
+                    }))
+
+
+
+                }
+            }
+        })
+    } else {
+        res.status(400).send(JSON.stringify({
+            code: "err",
+            message: "invalid request"
+        }))
+    }
+
+    
+
+
+
+})
+
+
+
+app.get("/getUser", (req,res) => {
+    authenticateUser(req).then((id) => {
+        if (id === "No user found") {
+            res.status(403).send(JSON.stringify({
+                code: "err",
+                message: "invalid request"
+            }))
+        } else {
+            User.findOne({uuid: id}).then((user,err) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send(JSON.stringify({
+                        code: "err",
+                        message: "invalid request"
+                    }))
+                } else {
+                    if (user!==null) {
+                        const decryptedUser = {
+                            name: cmod.decrypt(user.name),
+                            email: cmod.decrypt(user.email),
+                        }
+
+                        Campaign.find({uuid: user.uuid}).then((listings, err) => {
+                            if (err) {
+                                console.log(err)
+                                res.status(500).send(JSON.stringify({
+                                    code: "err",
+                                    message: "invalid request"
+                                }))
+                            } else {
+                                
+                                const things = []
+
+                                listings.map(async(listing, i) => {
+                                    let decryptedDetails = {
+                                        jobListing: cmod.decrypt(listing.jobListing),
+                                        numberOfApplicants: listing.numberOfApplicants,
+                                        uuid: user.uuid,
+                                    }
+
+                                    const allApplicants = []
+
+                                    listing.applicants.map((applicant,i) => {
+
+                                        
+
+
+                                        allApplicants.push({
+                                            id: i,
+                                            name: cmod.decrypt(applicant.name),
+                                            email: cmod.decrypt(applicant.email),
+                                            phone: cmod.decrypt(applicant.phone),
+                                            role: cmod.decrypt(applicant.role),
+                                            experience: cmod.decrypt(applicant.experience),
+                                            skills: applicant.skills,
+                                            compatibilityScore: applicant.compatibilityScore,
+                                            status: cmod.decrypt(applicant.status)
+                                        })    
+
+                                              
+                                        
+                                    })
+
+                                    decryptedDetails["applicants"] = allApplicants
+                                    things.push(decryptedDetails)
+
+
+
+                                })
+
+                                res.status(200).send(JSON.stringify({
+                                    code: "ok",
+                                    message: {user: decryptedUser, campaigns: things}
+                                }))
+                            }
+                        })
+
+
+
+                    }
+                }
+            })
+        }
+    })
+})
+
+
+
+function processCsv(plainText) {
+    // Full Name, Email, Phone Number, Current Role, Years of Experience, Skills
+    // 6 different ideas
+    const applicants = []
+
+
+    
+        //     id: 1,
+        //     name: "Sarah Johnson",
+        //     email: "sarah.j@email.com",
+        //     phone: "+1234567890",
+        //     role: "Senior Frontend Developer",
+        //     experience: "5 years",
+        //     skills: ["React", "TypeScript", "Node.js", "AWS"],
+        //     compatibilityScore: 92,
+        //     status: "new"
+        //   },
+    
+    
+    plainText.split('\n').forEach((content,i) => {
+
+        const skills = []
+        // console.log(content)
+        if (i!==0) {
+            const list = content.split(",")
+            // console.log("heres the list",list)
+            // console.log(applicants)
+       
+                
+                list[5].split(";").forEach((val) => {
+                    
+                    console.log(val)
+                    skills.push(val)
+                })
+                
+
+                //     id: 1,
+                        //     name: "Sarah Johnson",
+                        //     email: "sarah.j@email.com",
+                        //     phone: "+1234567890",
+                        //     role: "Senior Frontend Developer",
+                        //     experience: "5 years",
+                        //     skills: ["React", "TypeScript", "Node.js", "AWS"],
+                        //     compatibilityScore: 92,
+                        //     status: "new"
+                        //   },
+            
+                    applicants.push({
+                        name: cmod.encrypt(list[0]),
+                        email: cmod.encrypt(list[1]),
+                        phone: cmod.encrypt(list[2]),
+                        role: cmod.encrypt(list[3]),
+                        experience: cmod.encrypt(list[4]),
+                        skills: skills,
+                        status: cmod.encrypt("new"),
+
+                    })
+
+            
+
+            
+        
+        }
+       
+        // console.log(content[5].split(";"))
+        
+    })
+
+    return applicants
+
+
+}
+
+
+app.post("/uploadListing", async(req,res) => {
+
+    authenticateUser(req).then(async(id) => {
+        if (id === "No user found") {
+            res.status(403).send(JSON.stringify({
+                code: "err",
+                message: "invalid request"
+            }))
+        } else {
+            const {positionName, criteria, csvFile, manual} = req.body;
+
+
+            if (!manual) {
+                // when recruiters do the batch upload
+                console.log(csvFile.split(",").length)
+                console.log("bool value", csvFile.split(",").length % 2)
+                if ((csvFile.split(",").length >6) && (positionName.length >0) && (criteria.length>0) ) {
+                    const applicants = await processCsv(csvFile);
+
+                    console.log(applicants)
+                    
+
+                    // uuid: {
+                    //     type: String,
+                    //     index: true
+                    // },
+                    // jobListing: {
+                    //     type: String,
+                    //     unique: false,
+                    // },
+                    // applicants: {
+                    //     type: Array,
+                    //     unique: false
+                    // },
+                    // numberOfApplicants: {
+                    //     type: Number,
+                    //     unique: false,
+                    // }
+                
+
+
+
+
+
+                    
+                        
+
+                    const newCampaign = new Campaign({
+
+                        uuid: id,
+                        jobListing: cmod.encrypt(positionName.toLowerCase()),
+                        criteria: criteria,
+                        applicants: applicants,
+                        numberOfApplicants: applicants.length,
+
+
+
+                    })
+                    newCampaign.save();
+                    
+
+
+                    res.status(200).send(JSON.stringify({
+                        code: "ok",
+                        message: "success"
+                    }))
+
+
+
+
+
+                }
+
+
+
+
+            }
+    
+        }
+        })
+})
+
+
+
 
 
 
@@ -144,6 +630,15 @@ app.get("/", (req,res) => {
     res.send("hello world");
 
 })
+
+
+
+
+
+
+
+
+
 
 
 
